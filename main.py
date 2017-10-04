d3e9ff6fb992e857e401ec9f589daf9221c62737
Amit Kumar Jaiswal <amitkumarj441@gmail.com>
2017-10-04 21:35:19 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+# Python package python-gi
+
+import signal
+from indicator.indicator import Indicator
+from indicator.coinbase import Coinbase
+
+if __name__ == '__main__':
+    signal.signal(signal.SIGINT, signal.SIG_DFL)
+    client = Coinbase()
+    app = Indicator(client)
+    app.run()
